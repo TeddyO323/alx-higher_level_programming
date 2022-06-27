6f41ee26cb4717ace69ea8c7efdadb55c4ae1c04
TeddyO323 <theoriginalsomosh@gmail.com>
2022-06-28 00:37:14 +0300

    String representation

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Module to create a Rectangle Object
+"""
+
+
+class Rectangle:
+    """This is Rectangle class
+    """
+
+    def __init__(self, width=0, height=0):
+        """This is __init__ method
+        Is to initialize variables
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
